c9222fc44555b199946adcb43127849193605334
RamyaGit12 <sasidaran.jeyachandran@gmail.com>
2025-01-26 19:18:35 +0000

    Weather App

---
+++ Weather.py
@@ -0,0 +1,20 @@
+# app.py
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/weather', methods=['GET'])
+def get_weather():
+    city = request.args.get('city')
+    api_key = '5963a7d96524ff80564bc5fdf1564e27'
+    base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(base_url)
+    data = response.json()
+    if data['cod'] != '404':
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'City not found'}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
